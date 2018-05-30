d47def09c70c80687b4e98fd2168f91981a673dc
Jacob Olsby <jacob@olsby.com>
2018-05-29 19:41:39 -0700

    Here is a comment for the user to see.

---
+++ myapp/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url, include
+from myapp.views import list_view, detail_view, UserViewSet, GroupViewSet 
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+
+urlpatterns = [
+    url(r'^posts/(?P<post_id>\d+)/$', detail_view, name="blog_detail"),
+    url(r'^$', list_view, name="blog_index"),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+]
+
---
+++ myapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+
+
+from myapp.models import Post
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from myapp.serializers import UserSerializer, GroupSerializer
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('list.html')
+    context = {'posts': posts}
+    body = template.render({'posts': posts})
+    return HttpResponse(body, content_type="text/html")
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    context = {'post': post}
+    return render(request, 'detail.html', context)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
